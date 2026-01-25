d84d993324389255f4753db5a26fbc94db382fda
achourmohamedamine <mohamedamine.achour@insat.ucar.tn>
2026-01-25 14:01:09 +0100

    added Token Expiration Control

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -3,18 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { environment } from '../../../Commun/environments/environment';
 import { AuthResponse, LoginRequest, RegisterRequest, User, UserRole } from '../Models/auth.models';
-
+import { jwtDecode } from 'jwt-decode';
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
     private http = inject(HttpClient);
     private apiUrl = `${environment.apiUrl}/auth`;
+    private logouttimer:any;
 
     // State management using Signals
     private _currentUser = signal<User | null>(null);
     readonly currentUser = this._currentUser.asReadonly();
-    readonly isAuthenticated = computed(() => !!this._currentUser());
+    readonly isAuthenticated = computed(() => {
+        const user = this._currentUser();
+        return !!user && !this.isTokenExpired();
+    });
     readonly isAdmin = computed(() => this._currentUser()?.role === UserRole.ADMIN);
     readonly isOrganizer = computed(() => this._currentUser()?.role === UserRole.ORGANIZER);
 
@@ -53,6 +57,10 @@ export class AuthService {
         localStorage.removeItem('access_token');
         localStorage.removeItem('user');
         this._currentUser.set(null);
+        if (this.logouttimer)
+        {
+            clearTimeout(this.logouttimer);
+        }
     }
 
     /**
@@ -62,6 +70,8 @@ export class AuthService {
         localStorage.setItem('access_token', token);
         localStorage.setItem('user', JSON.stringify(user));
         this._currentUser.set(user);
+        const decodedToken :any=jwtDecode(token);
+        this.autoLogout(decodedToken.exp*1000);
     }
 
     /**
@@ -71,13 +81,18 @@ export class AuthService {
         const token = localStorage.getItem('access_token');
         const userStr = localStorage.getItem('user');
 
-        if (token && userStr) {
+        if (token && userStr && !this.isTokenExpired()) {
             try {
                 const user = JSON.parse(userStr);
                 this._currentUser.set(user);
+                const decodedToken :any=jwtDecode(token);
+                this.autoLogout(decodedToken.exp*1000);
             } catch (e) {
                 this.logout();
             }
+        }
+        else {
+            this.logout();
         }
     }
 
@@ -86,5 +101,36 @@ export class AuthService {
      */
     getToken(): string | null {
         return localStorage.getItem('access_token');
+    }
+
+    isTokenExpired(): boolean {
+    const token = this.getToken();
+    if (!token) return true;
+    try {
+        const decoded: any = jwtDecode(token);
+        const currentTime = Math.floor(Date.now() / 1000);
+        return decoded.exp < currentTime;
+    } catch {
+        return true; 
+    }
+
+}
+    private autoLogout(ExpirationDate:number) : void{
+        const now=Date.now();
+        const delay=ExpirationDate-now;
+        if (this.logouttimer)
+        {
+            clearTimeout(this.logouttimer);
+        }
+        if (delay>0) {
+            this.logouttimer=setTimeout(()=>{
+                this.logout();
+            },delay);
+        } 
+        else {
+            this.logout();
+        }
+
+
     }
 }
