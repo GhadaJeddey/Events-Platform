ed6ed840ecabeb1eb272d5fdcd2934e330c44f19
GhadaJeddey <ghada.jeddey@insat.ucar.tn>
2026-01-29 13:17:44 +0100

    save local changes befoe commit

--- frontend/src/app/Models/organizer.ts
+++ frontend/src/app/Models/organizer.ts
@@ -1,11 +1,8 @@
-import { User } from './User'; 
-
 export interface Organizer {
-  id: string;
-  name: string;        
-  description?: string;
-  website?: string;
-  isVerified: boolean;
-  
-  user?: User;          
+    id: string;
+    name: string;
+    description?: string;
+    website?: string;
+    isVerified: boolean;
+    eventsCount?: number;
 }
